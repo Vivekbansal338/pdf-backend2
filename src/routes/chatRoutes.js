59f50cf10ba8238af0ff0f5ee15b3af667d8ec01
VivekBansalBoxinall <vivekbansal@boxinallsoftech.com>
2025-03-12 08:55:18 +0530

    refactored

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   chatHandler,
-  chatHandlerStream,
   getChatHistoryByDocument,
 } from "../controllers/chatController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
@@ -9,7 +8,6 @@ import authMiddleware from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/chat", authMiddleware, chatHandler);
-router.post("/chatstream", authMiddleware, chatHandlerStream);
 router.get("/history/:documentId", authMiddleware, getChatHistoryByDocument);
 
 export default router;
